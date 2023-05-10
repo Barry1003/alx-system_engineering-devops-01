376f919c8f54506a07f6dbc04e8ce6798309a8af
Barry1003 <isholausamah@gmail.com>
2023-05-10 09:05:10 +0100

    update

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent header to avoid errors related to Too Many Requests
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data["data"]["subscribers"]
+            return subscribers
+        else:
+            return 0
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return 0
